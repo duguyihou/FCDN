7e001b244ef9dacce92d16f75aa82ca2023b8fbc
josh <jshkeeg@gmail.com>
2020-06-17 00:50:40 +1000

    Further config

--- src/auth/services.ts
+++ src/auth/services.ts
@@ -1,14 +1,7 @@
-import express from 'express';
-
 export async function ensureLoggedIn(ctx, next) {
     await next();
   }
   
-export function getUserId(ctx) {
-  return '123321';
-}
-
-const PORT = process.env.PORT || 3000;
-const app = express();
-app.get('/', (req, res) => res.send('Hello World!'));
-app.listen(PORT, () => console.log(`Example app listening on port ${PORT}!`));
+  export function getUserId(ctx) {
+    return '123321';
+  }
